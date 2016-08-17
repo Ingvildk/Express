21ddaa6a69a04a6f5ec39fbf9757794df40efed8
Ingvild <ingvild_kongsgaard@hotmail.com>
2016-08-17 14:22:31 -0700

    Use the new request and response methods provided thorugh Express to blacklist an evil IP address

--- middlew/blacklistingIP.js
+++ middlew/blacklistingIP.js
@@ -1,1 +1,14 @@
-dd
+var express = require("express");
+var app = require("app");
+
+var EVIL_IP = "123.45.67.89";
+
+app.use(function(request, response, next) {
+	if (request.ip === EVIL_IP) {
+		response.status(401).send("Not allowed!");
+	} else {
+		next();
+	}
+});
+
+app.listen(3000);
