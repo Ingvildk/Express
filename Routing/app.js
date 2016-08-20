d420d170b7dfbce9c263c1ebbc0f0894a4b621a1
Ingvild <ingvild_kongsgaard@hotmail.com>
2016-08-20 14:21:47 -0700

    Define a route that looks for ranges; that is, if you visit /users/100-500

--- Routing/app.js
+++ Routing/app.js
@@ -6,15 +6,23 @@ app.get("/olivia", function(req, res) {
 	res.send("Welcome to Olivia's homepage");
 });
 
+// Define a route that looks for ranges; that is, if you visit /users/100-500
+// you can see a list of users from IDs 100 to 500
+app.get(/^\/users\/(\d+)-(\d+)$/, function(req, res) {
+	var startId = parseInt(req.params[0], 10);
+	var endId = parseInt(req.params[1], 10);
+	res.send("/users/" + startId + "-" + endId);
+	});
+
+/*
 // 	The page can only be rendered if the number after /users is an integer, thanks to regular expression
-// defines the route URLs and captures digits using a regular expression
+// 
 app.get(/^\/users\/(\d+)$/, function(req, res) {
 // accesses parameters by their ordinality	
 	var userId = parseInt(req.params[0], 10);
 	res.send("/users/" + userId);
 });
 
-/*
 app.get("/users/:userid", function(req, res) {
 	console.log("/users/:userid");
 
