8e3e9d75ee3eb537978a0c60d34379d696d6ccff
Ingvild <ingvild_kongsgaard@hotmail.com>
2016-08-20 18:07:54 -0700

    Serving static files from multiple directories, by using express.static

--- Static-File-Fun/simpleApp.js
+++ Static-File-Fun/simpleApp.js
@@ -4,15 +4,14 @@ var http = require("http");
 
 var app = express();
 
-var publicPath = path.resolve(__dirname, "public");
-app.use(express.static(publicPath));
-
 /*
-	Middleware can be mounted at a given prefix. You can make a middleware respond 
-	only if it starts with /offensive
+	How to serve static files from multiple directories
 */
+var staticPath = path.resolve(__dirname, "static");
 var photoPath = path.resolve(__dirname, "offensive-photos-folder");
-app.use("/offensive", express.static(photoPath));
+
+app.use(express.static(staticPath));
+app.use(express.static(photoPath));
 
 app.use(function(req, res) {
 	res.writeHead(200, {"Content-Type": "text/plain"});
